15ffdd20c49f9e8e42a39e6ef4b531e89cf47118
Angela Balaoro <angelabalaoro@gmail.com>
2019-09-04 13:54:45 +0800

    Updates

---
+++ precache-manifest.8162cbbcd19911ba85dc9fd9c3b4485c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ada527d8ed0761883e5b8c1654247eb",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "cf1f9e71520ffdf03267",
+    "url": "/react-tutorial/static/css/main.fc29d31f.chunk.css"
+  },
+  {
+    "revision": "90fda5286f658331d789",
+    "url": "/react-tutorial/static/js/2.3e6d1000.chunk.js"
+  },
+  {
+    "revision": "cf1f9e71520ffdf03267",
+    "url": "/react-tutorial/static/js/main.796fb799.chunk.js"
+  },
+  {
+    "revision": "6a2725a9f1b189562a4c",
+    "url": "/react-tutorial/static/js/runtime~main.dd218ccf.js"
+  }
+]);
